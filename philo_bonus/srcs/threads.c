88fcd264e60fd363522ed4eb669c8a9c61caa2e5
nsterk <nsterk@student.codam.nl>
2022-06-13 13:08:11 +0200

    forgot to add threads.c

---
+++ philo_bonus/srcs/threads.c
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   threads.c                                          :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: nsterk <nsterk@student.codam.nl>             +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2022/06/13 00:38:13 by nsterk        #+#    #+#                 */
+/*   Updated: 2022/06/13 02:11:45 by nsterk        ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <philo_bonus.h>
+#include <pthread.h>
+
+void				*monitoring_threads(void *arg)
+{
+	return (arg);
+}
